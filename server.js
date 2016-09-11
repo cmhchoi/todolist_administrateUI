8a39ae7ede4b3d3039c9575ad502782ff8b70f25
Chris Choi <cmhchoi@gmail.com>
2016-09-12 00:49:03 +0800

    port v2

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log('server is up at port ', PORT);
+  console.log('server is up at port ', port);
 });
